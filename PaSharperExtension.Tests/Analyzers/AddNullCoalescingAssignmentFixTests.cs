a2a3542c169e5f6bb409714aed9add6fbf6a9e4c
PaGrom <pagrom23@gmail.com>
2020-10-15 01:44:47 +0300

    [PossibleNullCoalescingAssignmentAnalyzer] Add quick fix tests

---
+++ PaSharperExtension.Tests/Analyzers/AddNullCoalescingAssignmentFixTests.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+using PaSharperExtension.Analyzers.PossibleNullCoalescingAssignment;
+
+namespace PaSharperExtension.Tests.Analyzers
+{
+    [TestNetCore30]
+    [TestFixture]
+    public sealed class AddNullCoalescingAssignmentFixTests : QuickFixTestBase<AddNullCoalescingAssignmentFix>
+    {
+        protected override string RelativeTestDataPath => @"Analyzers\PossibleNullCoalescingAssignmentQuickFixes";
+
+        [Test]
+        public void TestPossibleNullCoalescingAssignment_WithBlock() => DoNamedTest2();
+
+        [Test]
+        public void TestPossibleNullCoalescingAssignment_WithoutBlock() => DoNamedTest2();
+    }
+}
---
+++ PaSharperExtension.Tests/test/data/Analyzers/PossibleNullCoalescingAssignmentQuickFixes/PossibleNullCoalescingAssignment_WithoutBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaSharperExtension.Tests.test.data.Analyzers.PossibleNullCoalescingAssignmentQuickFixes
+{
+    class PossibleNullCoalescingAssignmentQuickFixes
+    {
+        void Foo(List<string> list)
+        {
+            if{caret} (list == null)
+                list = new List<string>();
+        }
+    }
+}
